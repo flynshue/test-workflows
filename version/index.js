83156ddfe34c070792f5fa45c17827f857d3cdfd
Felicia Lyn-Shue <flynshue@gmail.com>
2022-06-16 16:13:53 -0400

    test sha

--- version/index.js
+++ version/index.js
@@ -9,11 +9,13 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  const gitSha = github.context.payload["sha"]
+  console.log("git sha:" + gitSha)
 } catch (error) {
   core.setFailed(error.message);
 }
 
-const tagVersion = uuidv4().substring(0,4);
-console.log(tagVersion);
+// const tagVersion = uuidv4().substring(0,4);
+// console.log(tagVersion);

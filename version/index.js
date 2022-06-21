45c7fccea12f2e90dff48694efaedaf15fe5eac7
Felicia Lyn-Shue <flynshue@gmail.com>
2022-06-21 14:19:43 -0400

    more testing

--- version/index.js
+++ version/index.js
@@ -2,10 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const context = github.context
-  gitSha = context.sha.substring(0, 4)
-  console.log(`git sha: ${gitSha}`)
-  core.setOutput('version', gitSha)
+  const context = github.context;
+  let gitSourceRef = context.sha.substring(0, 4);
+  makeTagVersion(gitSourceRef)
+  core.setOutput('version', gitSourceRef);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function makeTagVersion(gitSourceRef) {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  return version = `v${year}-${month}-${day}-${gitSourceRef}`
 }

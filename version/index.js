bc68299b4745609d9578dde768046b6484117f67
Felicia Lyn-Shue <flynshue@gmail.com>
2022-06-21 11:36:44 -0400

    more testing

--- version/index.js
+++ version/index.js
@@ -1,22 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { v4: uuidv4 } = require('uuid');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
   const context = github.context
-  // console.log("git ref: " + context.ref)
-  console.log("git sha: " + context.sha.substring(0, 4))
+  gitSha = context.sha.substring(0, 4)
+  console.log(`git sha: ${gitSha}`)
+  core.setOutput('version', gitSha)
 } catch (error) {
   core.setFailed(error.message);
 }
-
-// const tagVersion = uuidv4().substring(0,4);
-// console.log(tagVersion);
